fe09502e1be1729cd2f334117b74fa75418d5c3b
nknighta <n.knight.pc0627@gmail.com>
2024-04-14 20:30:06 +0900

    💄 : style - snew style

--- index.js
+++ index.js
@@ -3,25 +3,31 @@ const base = document.getElementById('main');
 const heightOutput = document.querySelector("#height");
 const widthOutput = document.querySelector("#width");
 
+window.onload = () => {
+  console.log("start");
+  getPosition()
+};
+
+
 let map = new maplibregl.Map({
-    container: 'map',
-    style: 'https://tile.openstreetmap.jp/styles/osm-bright-ja/style.json', // 地図のスタイル
-    center: [139.8108103, 35.7100069], // 中心座標
-    zoom: 13, 
-    pitch: 0 // 傾き
+  container: 'map',
+  style: 'https://tile.openstreetmap.jp/styles/osm-bright-ja/style.json', // 地図のスタイル
+  center: [139.8108103, 35.7100069], // 中心座標
+  zoom: 13,
+  pitch: 30 // 傾き
 })
 
-function getPosition(){
-    navigator.geolocation.getCurrentPosition(
-      function(position){
-        let nowLatitude = position.coords.latitude;
-        let nowLongitude = position.coords.longitude;
-  
-        map.jumpTo({
-          center: [nowLongitude, nowLatitude],
-          zoom: 16
-        });
-      },
-    );
-  }
-  
+function getPosition() {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      let nowLatitude = position.coords.latitude;
+      let nowLongitude = position.coords.longitude;
+
+      map.jumpTo({
+        center: [nowLongitude, nowLatitude],
+        zoom: 16,
+        pitch: 30
+      });
+    },
+  )
+}
